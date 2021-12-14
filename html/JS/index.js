88061da1e5b8d6bbc12cc85cd52f6f031de431b2
hanh18 <htmhanh19@gmail.com>
2021-12-14 09:11:18 +0700

    show product items

--- html/JS/index.js
+++ html/JS/index.js
@@ -90,10 +90,55 @@ productList = [
     price: '40.000đ',
     discount: '-70%',
     img: 'https://salt.tikicdn.com/cache/200x200/ts/product/59/55/c3/efc937e8a0244a04f670ff316846a4f7.jpg'
+},
+{
+    productName: 'Sổ tay ghi chép mục tiêu: My Day My Life',
+    sold: 'Đã bán 1000+',
+    price: '40.000đ',
+    discount: '-70%',
+    img: 'https://salt.tikicdn.com/cache/200x200/ts/product/59/55/c3/efc937e8a0244a04f670ff316846a4f7.jpg'
+},
+{
+    productName: 'Sổ tay ghi chép mục tiêu: My Day My Life',
+    sold: 'Đã bán 1000+',
+    price: '40.000đ',
+    discount: '-70%',
+    img: 'https://salt.tikicdn.com/cache/200x200/ts/product/59/55/c3/efc937e8a0244a04f670ff316846a4f7.jpg'
 }
 ]
 
-function convertImg(img){
+// function imgProduct(img){
+//     return '<div class="product-img"> <img src="' + img + '" alt=""></div>';
+// }
+
+// function evaluate(){
+//     return '<div style="display: flex; align-items: center; margin: 0 0 4px 0;"><div style="display: flex;"><div style="position: relative;"><div style="display: flex; align-items: center; width: 15px; white-space: nowrap; left: 0px; top: 0px;"><img src="https://assets.dryicons.com/uploads/icon/svg/12715/7b07ac52-2bd3-41d3-bac8-766575d2e180.svg" alt=""><img src="https://assets.dryicons.com/uploads/icon/svg/12715/7b07ac52-2bd3-41d3-bac8-766575d2e180.svg" alt=""><img src="https://assets.dryicons.com/uploads/icon/svg/12715/7b07ac52-2bd3-41d3-bac8-766575d2e180.svg" alt=""><img src="https://assets.dryicons.com/uploads/icon/svg/12715/7b07ac52-2bd3-41d3-bac8-766575d2e180.svg" alt=""><img src="https://assets.dryicons.com/uploads/icon/svg/12713/0ba1c779-e08a-49d5-a61c-a35d478881c3.svg" alt=""></div></div></div></div>'
+// }
+
+// function soleProduct(sold){
+//     return '<div class="product-sold">' + sold + '</div>';
+// }
+
+// function priceProduct(price, discount){
+//     if(discount == ""){
+//         return '<div class="product-price"><div class="price-not-discount">' + price + '</div></div>';
+//     }
+
+//     return '<div class="product-price has-discount"><div class="price-discount__price">' + price + '</div><div class="price-discount__discount">' + discount + '</div></div>';
+// }
+
+// function convertCover(name, sold, price, discount){
+//     return '<div class="cover"><div class="product-name">' + name + '</div>' + evaluate() + soleProduct(sold) + priceProduct(price, discount) + '</div>';
+// }
+
+// function productItems (img, name, sold, price, discount){
+//     return '<div class="product-items" id="item">' + imgProduct(img) + convertCover(name, sold, price, discount) + '</div>'
+// }
+// var content = productItems (productList[0].img, productList[0].productName, productList[0].sold, productList[0].price, productList[0].discount);
+
+// document.getElementById('item').innerHTML = content;
+
+function imgProduct(img){
     return '<div class="product-img"> <img src="' + img + '" alt=""></div>';
 }
 
@@ -105,12 +150,6 @@ function soleProduct(sold){
     return '<div class="product-sold">' + sold + '</div>';
 }
 
-function convertCover(name, sold, price, discount){
-    // return '<div class="cover"><div class="product-name">' + name + '</div>' + evaluate() + '</div>';
-    return '<div class="cover"><div class="product-name">' + name + '</div>' + evaluate() + soleProduct(sold) + priceProduct(price, discount) + '</div>';
-
-}
-
 function priceProduct(price, discount){
     if(discount == ""){
         return '<div class="product-price"><div class="price-not-discount">' + price + '</div></div>';
@@ -119,8 +158,19 @@ function priceProduct(price, discount){
     return '<div class="product-price has-discount"><div class="price-discount__price">' + price + '</div><div class="price-discount__discount">' + discount + '</div></div>';
 }
 
+function convertCover(name, sold, price, discount){
+    return '<div class="cover"><div class="product-name">' + name + '</div>' + evaluate() + soleProduct(sold) + priceProduct(price, discount) + '</div>';
+}
 
-var content = convertImg(productList[0].img) + convertCover(productList[0].productName, productList[0].sold, productList[0].price, productList[0].discount);
+function productItems (img, name, sold, price, discount){
+    return '<div class="product-items" id="item">' + imgProduct(img) + convertCover(name, sold, price, discount) + '</div>'
+}
 
-document.getElementById('item').innerHTML = content;
+var arrProduct = productList.map(function(item){
+    return productItems(item.img, item.productName, item.sold, item.price, item.discount);
+})
+
+var content = arrProduct.join();
+
+document.getElementById('container-product-items').innerHTML = content;
 
