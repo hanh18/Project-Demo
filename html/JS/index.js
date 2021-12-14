3ec09bef6de63221bbf95370c6be1a5520aebd9e
hanh18 <htmhanh19@gmail.com>
2021-12-14 21:42:55 +0700

    hiển thị tên người dùng bằng local storage

--- html/JS/index.js
+++ html/JS/index.js
@@ -93,3 +93,12 @@ var content = arrProduct.join('');
 
 document.getElementById('container-product-items').innerHTML = content;
 
+//localstorage: get username
+var userName = localStorage.getItem('name');
+
+function loginUserName(name) {
+    return '<img with="30px" height="30px" src="https://salt.tikicdn.com/ts/upload/67/de/1e/90e54b0a7a59948dd910ba50954c702e.png" alt=""></img> ' + name;
+}
+var contentLoginUserName = loginUserName(userName);
+
+document.getElementById('login-username').innerHTML = contentLoginUserName;
--- html/JS/login.js
+++ html/JS/login.js
@@ -1,18 +1,29 @@
 accountList = [
     {
+        name: 'Nguyễn Văn A',
         email: 'abc@gmail.com',
         pass: 'Abcabcabc',
     },
     {
+        name: 'Trần Thị B',
         email: 'hello@123.com',
-        pass: 'abcabcabc',
-    },
+        pass: 'abcabcabc'
+    }
 ]
+
+//~ event "onclick = validationLogin()" in HTML
+var loginButtion = document.getElementById('login');
+loginButtion.onclick = validationLogin; //gắn sự kiện onclick với 1 hàm
+
+//store data use
+var storageKey = 'name';
 
 function validationLogin(){
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
+
     var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
     var messageEmail = '';
     var messagePassword = '';
     var message = '';
@@ -36,16 +47,17 @@ function validationLogin(){
 
     // kiểm tra xác thực pass & email
     if(messageEmail == "" && messagePassword == ""){
-        var check = accountList.find(function(account){
+        var check = accountList.find(account => {
             return account.email == email && account.pass == password;
         });
     
         if(check != undefined){
             //chuyển trang
+            localStorage.setItem(storageKey, check.name);
             window.location.href = "home.html";
         }
         else {
-            message = '* Email và mật khẩu không khớp !!!';
+            message = '* Sai email hoặc mật khẩu !!!';
         }
     }
     
