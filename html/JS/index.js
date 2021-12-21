5ff79af356155438a95621d6a3074dc06fab9f89
hanh18 <htmhanh19@gmail.com>
2021-12-21 16:14:50 +0700

    merge function content HTML

--- html/JS/index.js
+++ html/JS/index.js
@@ -24,7 +24,7 @@ axios.get(url).then(function(response){
 //Hàm triển khai thêm srcipt vào HTML
 function render(productList){
     var arrProduct = productList.map(function(item){
-        return productItems(item.img, item.productName, item.sold, item.price, item.discount);
+        return contentHtml(item.img, item.productName, item.sold, item.price, item.discount);
     })
     
     var content = arrProduct.join('');
@@ -33,32 +33,45 @@ function render(productList){
 }
 
 // các hàm chuyển thành script HTML 
-function imgProduct(img){
-    return '<div class="product-img"> <img src="' + img + '" alt=""></div>';
-}
+function contentHtml (img, name, sold, price, discount){
+    return `<div class="product-items">
 
-function evaluate(){
-    return '<div style="display: flex; align-items: center; margin: 0 0 4px 0;"><div style="display: flex;"><div style="position: relative;"><div style="display: flex; align-items: center; width: 15px; white-space: nowrap; left: 0px; top: 0px;"><img src="https://assets.dryicons.com/uploads/icon/svg/12715/7b07ac52-2bd3-41d3-bac8-766575d2e180.svg" alt=""><img src="https://assets.dryicons.com/uploads/icon/svg/12715/7b07ac52-2bd3-41d3-bac8-766575d2e180.svg" alt=""><img src="https://assets.dryicons.com/uploads/icon/svg/12715/7b07ac52-2bd3-41d3-bac8-766575d2e180.svg" alt=""><img src="https://assets.dryicons.com/uploads/icon/svg/12715/7b07ac52-2bd3-41d3-bac8-766575d2e180.svg" alt=""><img src="https://assets.dryicons.com/uploads/icon/svg/12713/0ba1c779-e08a-49d5-a61c-a35d478881c3.svg" alt=""></div></div></div></div>'
-}
+    <div class="product-img"> 
+        <img src= ${img} alt="">
+    </div>
 
-function soleProduct(sold){
-    return '<div class="product-sold">' + sold + '</div>';
-}
+    <div class="cover">
+        <div class="product-name">
+            ${name}
+        </div>
 
-function priceProduct(price, discount){
-    if(discount == ""){
-        return '<div class="product-price"><div class="price-not-discount">' + price + '</div></div>';
-    }
+        <div style="display: flex; align-items: center; margin: 0 0 4px 0;">
+            <div style="display: flex;">
+                <div style="position: relative;">
+                    <div style="display: flex; align-items: center; width: 15px; white-space: nowrap; left: 0px; top: 0px;">
+                        <img src="https://assets.dryicons.com/uploads/icon/svg/12715/7b07ac52-2bd3-41d3-bac8-766575d2e180.svg" alt="">
+                        <img src="https://assets.dryicons.com/uploads/icon/svg/12715/7b07ac52-2bd3-41d3-bac8-766575d2e180.svg" alt="">
+                        <img src="https://assets.dryicons.com/uploads/icon/svg/12715/7b07ac52-2bd3-41d3-bac8-766575d2e180.svg" alt="">
+                        <img src="https://assets.dryicons.com/uploads/icon/svg/12715/7b07ac52-2bd3-41d3-bac8-766575d2e180.svg" alt="">
+                        <img src="https://assets.dryicons.com/uploads/icon/svg/12713/0ba1c779-e08a-49d5-a61c-a35d478881c3.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
 
-    return '<div class="product-price has-discount"><div class="price-discount__price">' + price + '</div><div class="price-discount__discount">' + discount + '</div></div>';
-}
-
-function coverProduct(name, sold, price, discount){
-    return '<div class="cover"><div class="product-name">' + name + '</div>' + evaluate() + soleProduct(sold) + priceProduct(price, discount) + '</div>';
-}
-
-function productItems (img, name, sold, price, discount){
-    return '<div class="product-items" id="item">' + imgProduct(img) + coverProduct(name, sold, price, discount) + '</div>'
+        <div class="product-sold">
+            ${sold}
+        </div>
+        
+        <div class="product-price has-discount">
+            <div class="price-discount__price">
+                ${price}
+            </div>
+            <div class="price-discount__discount">
+                ${discount}
+            </div>
+        </div>
+    </div></div>`
 }
 
 //chạy server: json-server --watch [tên file json] --port [cổng]
